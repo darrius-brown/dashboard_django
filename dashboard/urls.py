cc9199649cfb23caa5d6f2090cca6e756006c772
darriub15 <darriusabrown@gmail.com>
2023-04-11 21:47:20 -0700

    find client by user_id complete

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.CreateUser.as_view()),
-    path('clients/', views.ClientList.as_view(), name='client_read'),
-    path('clients/<int:pk>', views.ClientDetail.as_view(), name='client_detail'),
+    path('clients/<int:user_id>/', views.ClientListByUser.as_view(), name='client_read'),
+    path('clients/<int:user_id>/<int:pk>/', views.ClientDetail.as_view(), name='client_detail'),
     #search clients by supplier
     #search invoices by client
     #search invoices by supplier
--- dashboard/views.py
+++ dashboard/views.py
@@ -14,6 +14,17 @@ class ClientList(generics.ListCreateAPIView):
   queryset = Client.objects.all()
   permission_classes = [permissions.AllowAny]
 
+class ClientListByUser(generics.ListCreateAPIView,):
+  serializer_class = ClientSerializer
+  queryset = Client.objects.filter(supplier=1)
+  # permission_classes = [permissions.AllowAny]
+  
+  def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        queryset = Client.objects.filter(supplier=user_id)
+        return queryset
+
+
 class ClientDetail(generics.RetrieveUpdateDestroyAPIView):
   serializer_class = ClientSerializer
   queryset = Client.objects.all()
